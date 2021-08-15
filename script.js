c9f8376722610ea3b8c28d36b72418bb12e7f1f7
Akshay Bharadva <52954931+AkshayBharadva@users.noreply.github.com>
2021-08-15 16:37:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function setTime() {
 	const day = time.getDay();
 	const date = time.getDate();
 	const hours = time.getHours();
-	const hoursForClock = hours % 12;
+	const hoursForClock = hours % 12 === 0 ? 12 : hours % 12;
 	const minutes = time.getMinutes();
 	const seconds = time.getSeconds();
 	const ampm = hours > 12 ? 'PM' : 'AM';
